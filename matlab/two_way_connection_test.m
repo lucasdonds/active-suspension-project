e04ddc9cf3a2ebcfe2b3f9127bf7c117262b1153
lucas <lucasdondertman@gmail.com>
2019-10-25 09:40:32 -0400

    whoops

--- matlab/two_way_connection_test.m
+++ matlab/two_way_connection_test.m
@@ -1,8 +1,6 @@
 %% Main Code Section
 clear all; close all; clc;
-s=serial('/dev/cu.usbmodem143401','BaudRate',115200);
-set(s, 'Terminator', 'LF');
-fopen(s);
+startMacSerial('/dev/cu.usbmodem141401');
 
 x=fscanf(s);  %Read 
 x=strip(x);   %Remove new line from string (remove terminator)
@@ -31,7 +29,7 @@ y
 % y=fscanf(s);
 % x
 % y
-closePort('/dev/cu.usbmodem143401');
+closePort('/dev/cu.usbmodem141401');
 
 %% Run this section only if there is a problem with port
-closePort('/dev/cu.usbmodem143401');
+closePort('/dev/cu.usbmodem141401');
